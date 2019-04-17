326a4bc1ac2755aa71e6a48adac64f3ad90276a1
Guillaume Barluet De Beauchesne <guillaume.debeauchesne@withings.com>
2019-04-17 14:59:25 +0200

    improve exercise

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
 import './App.css';
 
 class App extends Component {
@@ -10,19 +8,41 @@ class App extends Component {
     this.state = {
       todos: [
         { done: false, text: 'Make input form available' },
-        { done: false, text: 'Add item' },
-        { done: false, text: 'Create component TodoItem' },
+        { done: false, text: 'Create TodoForm, TodoList and TodoItem components' },
         { done: false, text: 'Mark done a todo item' },
         { done: false, text: 'Remove todo item' },
       ],
     }
   }
 
+  renderTodoItems() {
+    const { todos } = this.state;
+
+    return todos.map((item, index) => {
+      return (
+        <li key={index}>
+          <span className={item.done ? 'done' : 'undone'}>&#9675;</span>
+          {item.text}
+          <span className="remove">X</span>
+        </li>
+      )
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <TodoForm addItem={this.addItem} />
-        <TodoList items={this.state.todos} markToDone/>
+        <form>
+          <input
+            type="text"
+            placeholder="Add a new todo"
+          />
+          <button type="submit">Add</button>
+        </form>
+
+        <ul onSubmit={this.handleSubmit}>
+          { this.renderTodoItems() }
+        </ul>
       </div>
     );
   }
